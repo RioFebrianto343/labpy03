369e3cdeefde9dcd184a9f3c0a170eb90aff4fc2
RioFebrianto343 <46512504+RioFebrianto343@users.noreply.github.com>
2019-02-19 16:10:26 +0700

    Add files via upload

---
+++ latihan02.py
@@ -0,0 +1,12 @@
+maks = 0
+
+
+while True:
+    a = int(input("Masukkan bilangan:"))
+    if a >= maks:
+          maks = a
+    if a == 0:
+          break
+
+print("Bilangan terbesar adalah:",maks)    
+ 
